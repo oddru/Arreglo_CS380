20f2e237021fc515fd5d5e34245b9e2139ac9be6
awiass.aeon <144144097+oddru@users.noreply.github.com>
2026-01-19 02:07:56 +0000

    import lox

---
+++ Arreglo_CS380/test.java
@@ -0,0 +1,13 @@
+lox/lox.java
+
+create a new file
+
+package com.craftinginterpreters.lox
+
+import java.io BufferedReader;
+import java.io IOException;
+import java.io InputStreamReader;
+import java.nio,charset,Charset;
+import java.io.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
