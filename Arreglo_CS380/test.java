f08f98447b69089ac6fab6b37382ddcd65abde30
awiass.aeon <144144097+oddru@users.noreply.github.com>
2026-01-19 02:13:10 +0000

    changes

--- Arreglo_CS380/test.java
+++ Arreglo_CS380/test.java
@@ -7,7 +7,7 @@ package com.craftinginterpreters.lox
 import java.io BufferedReader;
 import java.io IOException;
 import java.io InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.sCharset;
 import java.io.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
