74687dd5ef3fc3e0a65a3afbce778f2d2f9932a1
awiass.aeon <144144097+oddru@users.noreply.github.com>
2026-01-19 02:26:04 +0000

    code

--- Arreglo_CS380/test.java
+++ Arreglo_CS380/test.java
@@ -11,3 +11,37 @@ import java.nio.charset.sCharset;
 import java.io.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > ) {
+            System.out.println("Usage: jlox [script]");
+            System.exit((64));
+        } else if(args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+}
+
+private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+        System.out.print("> ");
+        String line = reader.readLine();
+        if (line = null) break;
+        run(line);
+    }
+}
+
+static void error(int line, String message) {
+    report(line, "", message);
+}
+
+private Static void report(int line, String where, String message){
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+}
